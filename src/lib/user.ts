fe97fe15a8f59149812058280f694603a8279bc5
masarumatsunaga <65140122+masarumatsunaga@users.noreply.github.com>
2025-03-15 15:05:44 +0900

    supabase修正

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -1,5 +1,7 @@
 import { User } from '@/domain/user';
-import { supabase } from '../utils/supabase';
+import 'dotenv/config';
+import { supabase } from '../utils/supabase.ts';
+import { UserSkill } from '@/domain/userSkill';
 
 // 全てのユーザーを取得する
 export async function getAllUsers(): Promise<User[]> {
@@ -21,4 +23,23 @@ export async function getUserById(userId: string): Promise<User | null> {
     throw new Error(error.message);
   }
   return data;
+}
+
+// ユーザーとユーザーに紐付くスキルを取得する
+export async function getUserWithSkills(userId: string): Promise<User | null> {
+  const user = await getUserById(userId);
+  if (!user) {
+    return null;
+  }
+
+  const { data: userSkills, error } = await supabase
+    .from('user_skills')
+    .select('*')
+    .eq('user_id', userId);
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  const skills = userSkills.map((userSkill) => userSkill.skill_id);
+  return { ...user, skills };
 }
